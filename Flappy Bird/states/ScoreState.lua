17ab2836a39e193fff7ad3299eb203bda7d1d0b0
SKTheu <63525232+sktheu@users.noreply.github.com>
2021-01-30 15:12:08 -0300

    Flappy Bird (Medals)
    Added medal´s feature in ScoreState.

--- Flappy Bird/states/ScoreState.lua
+++ Flappy Bird/states/ScoreState.lua
@@ -16,6 +16,8 @@ ScoreState = Class{__includes = BaseState}
 ]]
 function ScoreState:enter(params)
     self.score = params.score
+
+    self.medal = ScoreState:SetMedalSprite(self.score) -- Allocating the returned sprite in medal
 end
 
 function ScoreState:update(dt)
@@ -28,10 +30,30 @@ end
 function ScoreState:render()
     -- simply render the score to the middle of the screen
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Oof! You lost!', 0, 32, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 64, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+    -- renders the medal in the middle of the screen
+    love.graphics.draw(self.medal, VIRTUAL_WIDTH / 2 - self.medal:getWidth() / 2, (VIRTUAL_HEIGHT / 2 - self.medal:getHeight() / 2) - 8)
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 200, VIRTUAL_WIDTH, 'center')
+end
+
+--[[
+    Returns a medal sprite based on the player's score.
+]]
+function ScoreState:SetMedalSprite(score)
+    if score >= 30 then
+        return love.graphics.newImage('sprites/diamond_medal.png')
+    elseif score >= 15 then
+        return love.graphics.newImage('sprites/gold_medal.png')
+    elseif score >= 10 then
+        return love.graphics.newImage('sprites/silver_medal.png')
+    elseif score >= 5 then
+        return love.graphics.newImage('sprites/bronze_medal.png')
+    else
+        return love.graphics.newImage('sprites/without_medal.png')
+    end
 end
